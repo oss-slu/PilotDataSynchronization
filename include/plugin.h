4659880c58476ee72cb16ca63297ca04a5c3ae4d
Carly Hoover <carly.hoover@slu.edu>
2024-10-12 10:49:58 -0500

    Testing Files and CMakeLists.txt
    X
    x

---
+++ include/plugin.h
@@ -0,0 +1,13 @@
+#ifndef XPLANE_PLUGIN_H
+#define XPLANE_PLUGIN_H
+
+#include "XPLMDataAccess.h"
+
+extern XPLMDataRef elevationMslRef;
+extern XPLMDataRef elevationAglRef;
+extern XPLMDataRef airspeedRef;
+extern XPLMDataRef verticalVelocityRef;
+
+// Declare any functions you want to test here
+
+#endif // XPLANE_PLUGIN_H
