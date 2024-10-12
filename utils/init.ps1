324c64faabf78cddfe3300ae9d76c17d6657a94f
Henry Meiners <henry.meiners@slu.edu>
2024-10-12 11:58:39 -0500

    Changed Download Location for '.\utils\init.ps1'
    The 'utils\init.ps1' script was creating the library and SDK folder inside the 'utils' folder and thus would not be covered by the gitignore (at least on my machine). I changed the file path to download and install in the root of the project, which is consistent with what the '.gitignore' file says

--- utils/init.ps1
+++ utils/init.ps1
@@ -1,16 +1,16 @@
 # Run this script ONLY from the project root
 
 $dlurl="https://developer.x-plane.com/wp-content/plugins/code-sample-generation/sdk_zip_files/XPSDK410.zip"
-$download_loc="./sdk.zip"
+$download_loc="../sdk.zip"
 
 # Create lib folder if it doesn't exist
-New-Item ./lib/SDK -ItemType Directory -Force
+New-Item ../lib/SDK -ItemType Directory -Force
 
 # Download the SDK
 Invoke-WebRequest -Uri $dlurl -OutFile $download_loc
 
 # Extract SDK into the lib folder
-Expand-Archive $download_loc -DestinationPath "./lib/" -Force
+Expand-Archive $download_loc -DestinationPath "../lib/" -Force
 
 # Cleanup
 Remove-Item $download_loc
