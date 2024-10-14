214d724cf5b8256e140b443d16b2cac057d19e57
Henry Meiners <hrmeiners01@gmail.com>
2024-10-14 17:25:06 -0500

    undo init.ps1 change

--- utils/init.ps1
+++ utils/init.ps1
@@ -1,7 +1,7 @@
 # Run this script ONLY from the project root
 
 $dlurl="https://developer.x-plane.com/wp-content/plugins/code-sample-generation/sdk_zip_files/XPSDK410.zip"
-$download_loc="../sdk.zip"
+$download_loc="./sdk.zip"
 
 # Create lib folder if it doesn't exist
 New-Item ../lib/SDK -ItemType Directory -Force
