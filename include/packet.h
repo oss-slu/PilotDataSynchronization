0db94a1def9863fc091c788399b8fd59a677b058
Ali Elnour <ali.elnour@slu.edu>
2024-11-09 15:26:33 -0600

    created basic model for message passing between threads, for carly's branch

--- include/packet.h
+++ include/packet.h
@@ -3,7 +3,21 @@
 #include <string>
 #include <vector>
 
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
+#include <vector>
 using namespace std;
+
+vector<int> vec;
+mutex mvec;
+
+struct ThreadMessage;
+
+class ThreadQueue;
+
 string generate_packet(vector<string> vec);
 
 string output_xml();
--- src/packet.cpp
+++ src/packet.cpp
@@ -1,6 +1,51 @@
-#include "packet.h"
+#include "../include/packet.h"
 
 using namespace std;
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
+#include <vector>
+using namespace std;
+
+struct ThreadMessage {
+    const float* values_for_packet;
+    const bool end_execution_flag;
+
+    ThreadMessage(float const (&values_for_packet)[4], const bool tf) :
+        values_for_packet(values_for_packet),
+        end_execution_flag(tf) {};
+};
+
+class ThreadQueue {
+  private:
+    mutex m;
+    queue<ThreadMessage> q;
+
+  public:
+    int size() {
+        lock_guard<mutex> lock(m);
+        return q.size();
+    }
+
+    void push(ThreadMessage tm) {
+        lock_guard<mutex> lock(m);
+        q.push(tm);
+    }
+
+    ThreadMessage pop() {
+        lock_guard<mutex> lock(m);
+        ThreadMessage front = q.front();
+        q.pop();
+        return front;
+    }
+};
+
+void t() {
+    lock_guard<mutex> guard(mvec);
+}
+
 string generate_packet(vector<string> vec) {
     string output =
         accumulate(vec.begin(), vec.end(), string(""), [](string a, string b) {
