5bcdb92039cc58299c4737ac610b6c7238608113
Carly <carlyrhoover@gmail.com>
2025-02-13 11:49:26 -0600

    Tests run (not skipped)

--- tests/test.cc
+++ tests/test.cc
@@ -1,3 +1,13 @@
-int main() {
-  return 0;
+#include <gtest/gtest.h>
+#include <iostream>
+
+TEST(a, b) {
+    std::cout << "Test is running" << std::endl;
+    EXPECT_EQ(56, 56);
+}
+
+int main(int argc, char **argv) {
+    std::cout << "Main function started" << std::endl;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
