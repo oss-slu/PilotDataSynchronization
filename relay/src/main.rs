c59dd3a6b41f649816d62787af21c83053713e4c
ScorpionSG <karimizin@gmail.com>
2025-02-18 12:06:04 -0600

    ui init

--- relay/src/main.rs
+++ relay/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{
+    run,
+    widget::{column, text},
+    Element,
+};
+
+fn main() -> iced::Result {
+    iced::run("RELAY", update, view)
 }
+
+#[derive(Debug)]
+enum Message {}
+
+#[derive(Default)]
+struct State {}
+
+fn view(state: &State) -> Element<Message> {
+    text("Hello, world").into()
+}
+
+fn update(state: &mut State, message: Message) {}
