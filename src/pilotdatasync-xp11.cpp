2562b5888d7fe1ea4a2e72a78410d7fc5f214f22
Henry Meiners <henry.meiners@slu.edu>
2025-03-02 21:12:25 -0600

    Meson build system works for windows

--- src/pilotdatasync-xp11.cpp
+++ src/pilotdatasync-xp11.cpp
@@ -10,7 +10,7 @@
 #include <cmath>
 #include <string>
 #include <thread>
-#include <memory> // meson ci build is yelling at me
+#include <memory>  // Remove this when we remove threading from C++. This is here to keep meson happy
 
 // #include "packet.cpp"
 
