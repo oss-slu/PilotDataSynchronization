265a0b4f67408cc018aa08f1121ab1a263ae3f9a
DeviPriya-Kolla <devipriya.kolla@slu.edu>
2025-04-14 17:33:27 -0500

    Revert "Merge branch 'main' into gui-connection"
    This reverts commit dde149f5c13ec523d1e959fe6685f761c1ca8244, reversing
    changes made to 16b3e9f8accf4147d588e8c9288968b014f5fbcd.

--- relay/src/message.rs
+++ relay/src/message.rs
@@ -12,12 +12,3 @@ pub(crate) enum Message {
     ConnectionMessage
 
 }
-
-// Enum for messages from within the IPC connection thread
-pub(crate) enum IpcThreadMessage {
-    // Baton data to be sent over TCP
-    BatonData(String),
-
-    // signal that Baton is disconnecting from our server
-    BatonShutdown,
-}
